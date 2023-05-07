7ed268b801b490a449b31d2498fac1474b793a59
aminulislam2002 <aminulislamemon2002@gmail.com>
2023-05-07 11:09:22 -0700

    create client side post and Insert single user data

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,38 @@
 import "./App.css";
 
 function App() {
+  const handleAddUser = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const user = { name, email };
+    console.log(name, email, user);
+
+    fetch("http://localhost:5000/users", {
+      method: "POST",
+      headers: {
+        "content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => console.log(error));
+  };
+
   return (
-    <>
+    <div>
       <h1>Simple Crud</h1>
-    </>
+
+      <form onSubmit={handleAddUser}>
+        <input type="text" name="name" id="" />
+        <input type="email" name="email" id="" />
+        <input type="submit" value="Add User" />
+      </form>
+    </div>
   );
 }
 
